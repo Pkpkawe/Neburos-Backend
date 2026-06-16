e4ac28bee798cb4de88a24967f4d0faf7b914b26
Guilherme <jg21504956@gmail.com>
2026-06-15 21:49:24 -0300

    feat:adicionando arquivos basicos do sistema

---
+++ src/main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+
+app = FastAPI(
+    title="Neburos backend",
+    description="Api do sistema neburos, um sistema de gerenciamento financeiro open source e gratuito",
+    version="0.1.0",
+)
