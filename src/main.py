a246a2761a29c6b0c24d3865172141ee7318f22b
Guilherme <jg21504956@gmail.com>
2026-06-27 12:04:52 -0300

    feat: settings, .editorconfig e .env-example

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from logging import info
 
 from configs.loggin_config import setup_logging , logging_config
+from configs.settings import settings
 
 
 @asynccontextmanager
@@ -16,10 +17,11 @@ app = FastAPI(
     title="Neburos backend",
     description="Api do sistema neburos, um sistema de gerenciamento financeiro open source e gratuito",
     version="0.1.0",
-    lifespan=life_span
+    lifespan=life_span,
+    debug=settings.DEBUG
 )
 
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_config=logging_config)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, log_config=logging_config,reload=settings.DEBUG)
